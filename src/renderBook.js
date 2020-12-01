43ebed7cd8e17d606e84e0fd7d340a05e78fe131
Ivan <ivantbv@gmail.com>
2020-12-01 22:20:08 +0500

    implemented a delete button on each book card that removes the corresponding  book both from the DOM and myLibrary array

--- src/renderBook.js
+++ src/renderBook.js
@@ -1,7 +1,10 @@
-import {myLibrary} from "/src/addingBooks.js"
+import { myLibrary } from "/src/addingBooks.js"
+import {counter} from "/src/creatingBooks.js"
 
 const appendBookDiv = document.querySelector('.append-book')
+
 const render = () => {
+    let libraryCopy = myLibrary
     //creating the divs in the book's card
     const bookCard = document.createElement('div');
     bookCard.classList.add('book-card')
@@ -15,22 +18,52 @@ const render = () => {
     const bookRead = document.createElement('div')
     bookRead.classList.add('book-read')
 
-    console.log(myLibrary, 'from render')
+    const deleteBookButton = document.createElement('button');
+    deleteBookButton.classList.add('delete-book')
+    deleteBookButton.textContent = 'X';
+
     //loop through the library array to get the text content from its values
-    myLibrary.forEach(book => {
+    libraryCopy.forEach(book => {
         bookTitle.textContent = book.title;
         bookAuthor.textContent = book.author;
         bookPages.textContent = book.pages;
         bookRead.textContent = book.read;
         //the books will most likely stack on each other
         //clear the DOM from the book cards first and re-render
-        console.log(book.title, 'from forEach render');       
     });
+
+    // for (let i =0; i < myLibrary.length; i++) {
+    // deleteBookButton.setAttribute('data-id', myLibrary[i].id)
+    // console.log(deleteBookButton), 'from for loop setting data attr';
+    // }
+    deleteBookButton.setAttribute('data-id', counter)
+
+    deleteBookButton.addEventListener('click', (e) => {
+       for (let i=0; i < libraryCopy.length; i++) {
+            if (e.target.getAttribute('data-id') == libraryCopy[i].id) {
+                libraryCopy.splice([i], 1)
+            }
+       }
+                //libraryCopy.splice(libraryCopy.indexOf(e.target.getAttribute('data-id')), 1);
+
+        //myLibrary.splice(myLibrary.indexOf(e.target.getAttribute('data-id')), 1);
+           // if (myLibrary[i].id === e.target.getAttribute('data-id')) {
+          //  libraryCopy = libraryCopy.filter(book => book.id !== e.target.getAttribute('data-id'))
+                
+        e.target.parentElement.remove();
+     //let filtered = myLibrary.filter(book => book.id !== e.target.getAttribute('data-id'))
+        console.log(libraryCopy, 'from del btn');
+    })
+
     bookCard.appendChild(bookTitle)
     bookCard.appendChild(bookAuthor)
     bookCard.appendChild(bookPages)
     bookCard.appendChild(bookRead);
+    bookCard.appendChild(deleteBookButton);
     appendBookDiv.appendChild(bookCard);
+
+    console.log(libraryCopy, 'library copy')
+    console.log(deleteBookButton, 'del btnrender');
 }
 
-export {render}
+export { render }
