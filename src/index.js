284d03062664fa3a5b2c1885ceb6cf1571c3688c
Ivan <ivantbv@gmail.com>
2020-12-11 22:47:01 +0500

    removed the loop from the render function and added an argument to it. Added a local storage so that books stay on the page when added even after a refresh or tab close

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import { DomManipulation } from "/src/domManipulation.js"
-import { addBookToLibrary } from "/src/addingBooks.js"
-import { render } from "/src/renderBook.js"
+import { addBookToLibrary, render, createBook } from "/src/addingBooks.js"
+//import { render } from "/src/renderBook.js"
 import {myLibrary} from "/src/addingBooks.js"
+
 
 const domManip = new DomManipulation()
 domManip.openModal();
@@ -10,7 +11,8 @@ const addBookButton = document.querySelector('.add-book')
 addBookButton.addEventListener('click', () => {
 
     addBookToLibrary()
-    render()
+    // render()
+    createBook();
     console.log(myLibrary)
 });
 
