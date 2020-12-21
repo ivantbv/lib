043efbe5c0802680f654473d833fa96275e59d71
Ivan <ivantbv@gmail.com>
2020-12-21 19:27:17 +0500

    changed the read button to a checkbox and styled it with red - for book not read and green - book read

--- src/renderBook.js
+++ src/renderBook.js
@@ -23,14 +23,29 @@ const render = bookInfo => {
     bookPic.classList.add('book-pic');
     const bookRead = document.createElement('div')
     bookRead.classList.add('book-read')
+    const readDiv = document.createElement('div');
 
     const deleteBookButton = document.createElement('button');
     deleteBookButton.classList.add('delete-book', 'flex')
     deleteBookButton.textContent = 'X';
 
-    const toggleReadButton = document.createElement('button');
-    toggleReadButton.classList.add('read-button')
-    toggleReadButton.textContent = 'Read Status'
+    // const toggleReadButton = document.createElement('button');
+    // toggleReadButton.classList.add('read-button')
+    // toggleReadButton.textContent = 'Read Status'
+
+    const toggleReadButton = document.createElement('input');
+    toggleReadButton.type = 'checkbox'
+    toggleReadButton.classList.add('read-check')
+    if (bookInfo.read === 'Read') {
+        toggleReadButton.checked = true;
+    } else {
+        toggleReadButton.checked = false;
+    }
+
+    readDiv.appendChild(bookRead)
+    readDiv.appendChild(toggleReadButton)
+
+    readDiv.classList.add('read-div')
 
     //get the text content from the library array from createBook function
     bookTitle.textContent = bookInfo.title;
@@ -49,19 +64,29 @@ const render = bookInfo => {
 
     setLocalStorage();
 
-    // deleteBookButton.setAttribute('data-id', counter)
-
     deleteBookButton.addEventListener('click', (e) => {
         myLibrary.splice(myLibrary.indexOf(bookInfo),1);
         createBook(myLibrary)    
         setLocalStorage()
     })
 
-    toggleReadButton.addEventListener('click', (e) => {
-         if (bookInfo.read === 'Read') {
+    // toggleReadButton.addEventListener('click', (e) => {
+    //      if (bookInfo.read === 'Read') {
+    //         bookInfo.read = 'Not read'
+    //     } else if (bookInfo.read === 'Not read') {
+    //         bookInfo.read = 'Read'
+    //     }
+
+    //     createBook(myLibrary)
+    //     setLocalStorage()
+    // })
+    toggleReadButton.addEventListener('click', function() {
+        if (bookInfo.read === 'Read') {
             bookInfo.read = 'Not read'
+            console.log(bookInfo.read)
         } else if (bookInfo.read === 'Not read') {
             bookInfo.read = 'Read'
+            console.log(bookInfo.read)
         }
 
         createBook(myLibrary)
@@ -72,11 +97,11 @@ const render = bookInfo => {
 
     if (bookInfo.author !== 'by ') { bookCard.appendChild(bookAuthor); }
     if (bookInfo.pages !== ' pages') {bookCard.appendChild(bookPages); }
-    bookCard.appendChild(bookRead);
+    bookCard.appendChild(readDiv);
     if (bookInfo.link) { bookCard.appendChild(bookLink); }
     bookCard.appendChild(bookPic);
     bookCard.appendChild(deleteBookButton);
-    bookCard.appendChild(toggleReadButton);
+    //bookCard.appendChild(toggleReadButton);
 
     appendBookDiv.appendChild(bookCard);
 }
